0ab83716e4f3e706024a1965e63115b77577a6e8
hummingengineer <hummingengineer@gmail.com>
2020-02-06 23:59:18 +0900

    Router to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { useState, useCallback } from 'react'
-import NewsList from './components/NewsList'
-import Categories from './components/Categories'
+import { Route } from 'react-router-dom'
+import NewsPage from './pages/NewsPage'
 
-function App() {
-  const [category, setCategory] = useState('all')  // category 상태를 관리하는 useState 함수
-  const onSelect = useCallback(category => setCategory(category), [])  // category 값을 업데이트하는 onSelect 함수. category는 c.name이다.
-
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect}/>
-      <NewsList category={category}/>
-    </>
-  )
+function App () {
+  return <Route path="/:category?" component={NewsPage}/>
 }
 
 export default App
