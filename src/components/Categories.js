ac9cb37ab1e64c6ed2187e42b4b1e4e0a336f29f
hummingengineer <hummingengineer@gmail.com>
2020-02-07 01:02:20 +0900

    Add NavLink to Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,8 @@
+/* Categories에서 기존의 onSelect 함수를 호출하여 카테고리를 선택하고, 선택된 카테고리에 다른 스타일을 주는 기능을 NavLink로 대체 */
+
 import React from 'react'
-import styled, { css } from 'styled-components' // 단순 변수의 형태가 아니라 여러 줄의 스타일 구문을 조건부로 설정해야 하는 경우에는 css를 불러와야 한다.
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
 
 const categories = [
   { name: 'all', text: '전체보기' },  // name은 실제 카테고리 값을 가리키고, text 값은 렌더링할 때 사용할 한글 카테고리를 가리킨다.
@@ -22,7 +25,9 @@ const CategoriesBlock = styled.div`
   }
 `;
 
-const Category = styled.div`
+// div, a, button, input 처럼 일반 HTML 요소가 아닌 특정 컴포넌트에 styled-components를 사용할 때는 styled(컴포넌트이름)`` 과 같은 형식을 사용한다.
+// NavLink는 현재 경로와 Link에서 사용하는 경로가 일치하는 경우 특정 스타일 혹은 CSS 클래스를 적용할 수 있는 컴포넌트이다.
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
@@ -34,28 +39,39 @@ const Category = styled.div`
     color: #495057;
   }
 
-  /* styled-components에서는 조건부 스타일링을 간단하게 props로도 처리할 수 있다. */
-  /* 다음 코드는 active 값이 true일 때, 특정 스타일을 부여해 준다. */
-  ${props =>
-    props.active && css`                /* 스타일 코드 여러 줄을 props에 따라 넣어 주어야 할 때는 CSS를 styled-components에서 불러와야 한다. CSS를 사용하지 않아도 작동은 하지만 이 경우 해당 내용이 그저 문자열로만 취급되고, Tagged 템플릿 리터럴이 아니기 때문에 함수를 받아 사용하지 못해 해당 부분에서는 props 값을 사용하지 못한다. */
-      font-weight: 600;                 /* 만약 조건부 스타일링을 할 때 넣는 여러 줄의 코드에서 props를 참조하지 않는다면 굳이 CSS를 불러와서 사용하지 않아도 상관없지만, props를 참조한다면, 반드시 CSS로 감싸 주어서 Tagged 템플릿 리터럴을 사용해 주어야 한다. */
-      border-bottom: 2px solid #22b8cf;
-      color: #22b8cf;
-      &:hover {
-        color: #3bc9db;
-      }
-  `}
+  /* active는 마우스로 클릭한 상태 */
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
 
   & + & {
     margin-left: 1rem;
   }
 `;
 
-function Categories ({ onSelect, category }) {
+// NavLink로 만들어진 Category 컴포넌트에 to 값은 "/카테고리이름"으로 설정해주었다.
+// 카테고리 중에서 전체보기의 경우는 예외적으로 "/all" 대신에 "/"로 설정
+// to 값이 "/" 를 가리키고 있을 때는 exact 값을 true로 해 주어야 한다.
+// 이 값을 설정하지 않으면, 다른 카테고리가 선택되었을 때도 "전체보기" 링크에 active 스타일이 적용되는 오류가 발생한다.
+
+// exact 값을 true로 해주지 않으면, /business 경로로 들어가더라도 / 경로가 같이 나오게 된다. 이는 /business 경로가 / 규칙에도 일치하기 때문에 발생한 현상이다.
+// exact를 사용하면, 매칭이 될 경우 하위 라우트 설정을 보지 않게 된다.
+// /all로 가게 되면 to 조건식에 의해 "/" 경로로 가게 되고, exact 조건식에 의해 true가 되기 때문에 "/" 경로 하나만 보여주고 하위 라우트 설정을 보지 않게 된다.
+
+// NavLink에서 링크가 활성화되었을 때의 스타일을 적용할 때는 activeStyle 값을, CSS 클래스를 적용할 때는 activeClassName 값을 props로 넣어주면 된다.
+// react-router의 Link에 activeClassName 속성을 주면 Link가 활성화되었을 때 자동으로 activeClassName이 className으로 적용된다.
+// activeClassName는 지정해준 attribute가 활성화 될 경우 적용될 스타일 클래스를 선언해준다.
+// activeClassName는 렌더링 된 요소가 현재 URL과 일치할 때 클래스를 부여한다. 기본적으로 active 클래스를 부여한다. 기존 className 설정에 추가된다.
+function Categories () {
   return (
     <CategoriesBlock>
       {categories.map(c => (  // 컴포넌트를 map으로 렌더링할 때는 반드시 () 소괄호로 감싸주어야 한다. {} 중괄호 아니다.
-        <Category key={c.name} active={category === c.name} onClick={() => onSelect(c.name)}>{c.text}</Category>  // active는 마우스로 클릭한 상태. active는 props로 styled-component에게 전달된다.
+        <Category key={c.name} activeClassName="active" exact={c.name === 'all'} to={c.name === 'all' ? '/' : `/${c.name}`}>{c.text}</Category>  // 활성화 되었을 때 특정 클래스를 설정하고 싶다면 activeClassName 을 설정하시면 된다.
       ))}
     </CategoriesBlock>
   )
--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -4,6 +4,8 @@ import React from 'react'
 import Categories from '../components/Categories'
 import NewsList from '../components/NewsList'
 
+// URL 파라미터를 사용할 때는 라우트로 사용되는 컴포넌트에서 받아 오는 match라는 객체 안의 params 값을 참조한다.
+// match 객체 안에는 현재 컴포넌트가 어떤 경로 규칙에 의해 보이는지에 대한 정보가 들어있다.
 function NewsPage ({ match }) {
   const category = match.params.category || 'all'  // 카테고리가 선택되지 않았으면 기본값 all로 사용
 
