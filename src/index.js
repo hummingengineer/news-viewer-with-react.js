350f1a00aca476912b93fe4e2bc48486aa318189
hummingengineer <hummingengineer@gmail.com>
2020-02-06 23:56:42 +0900

    Create NewsPage.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'  // 기존에는 카테고리 값을 useState로 관리했는데, 이 값을 리액트 라우터의 URL 파라미터를 사용하여 관리하는 것으로 리팩토링.
 
 ReactDOM.render(
   <BrowserRouter>
