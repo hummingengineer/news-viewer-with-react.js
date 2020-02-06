94b49365c6bd8dd19adbf1e10a7c619a21ad6565
hummingengineer <hummingengineer@gmail.com>
2020-02-07 00:23:01 +0900

    Add comment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useState, useCallback } from 'react'
-import { Route } from 'react-router-dom'
-import NewsPage from './pages/NewsPage'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
+// 사용된 path에 /:category?와 같은 형태로 맨 뒤에 물음표 문자가 들어가 있는데, 이는 category 값이 선택적(optional)이라는 의미이다.
+// 즉, 있을 수도 있고 없을 수도 있다는 뜻.
+// category URL 파라미터가 없다면 전체 카테고리를 선택한 것으로 간주한다.
 function App () {
   return <Route path="/:category?" component={NewsPage}/>
 }
--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -1,13 +1,15 @@
+/* 리액트 라우터를 적용할 때 만들어야 할 페이지 */
+
 import React from 'react'
 import Categories from '../components/Categories'
 import NewsList from '../components/NewsList'
 
 function NewsPage ({ match }) {
-  const category = match.params.category || 'all'
+  const category = match.params.category || 'all'  // 카테고리가 선택되지 않았으면 기본값 all로 사용
 
   return (
     <>
-      <Categories/>
+      <Categories/>  {/* 현재 선택된 category 값을 URL 파라미터를 통해 사용할 것이므로 Categories 컴포넌트에서 현재 선택된 카테고리 값을 알려 줄 필요도 없고, onSelect 함수를 따로 전달해 줄 필요도 없다. */}
       <NewsList category={category}/>
     </>
   )
