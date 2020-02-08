3cd8aa5898f6fa85787b5cce33f5ac897cca7eb8
hummingengineer <hummingengineer@gmail.com>
2020-02-09 00:33:35 +0900

    Apply usePromise to NewsList

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import NewsItem from './NewsItem'
 import axios from 'axios'
+import usePromise from '../lib/usePromise'
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;  /* 박스의 크기를 어떤 것을 기준으로 계산할지를 정하는 속성. border-box 테두리를 기준으로 크기를 정한다. */
@@ -17,35 +18,19 @@ const NewsListBlock = styled.div`
 `;
 
 function NewsList ({ category }) {
-  const [articles, setArticles] = useState(null)  // 기본 값은 null
-  const [loading, setLoading] = useState(false)  // 요청이 대기 중일 때는 loading이라는 값이 true가 되고, 요청이 끝나면 loading 값이 false가 되어야 한다.
-
-  // useEffect를 사용하여 컴포넌트가 처음 렌더링 되는 시점에 API를 요청하면 된다.
-  // useEffect에 등록하는 함수에 async를 붙이면 안된다.
-  // useEffect에서 반환해야 하는 값은 뒷정리 함수이기 때문이다.
-  // 그러므로 useEffect 내부에서 async/await를 사용하고 싶다면, 함수 내부에 async 키워드가 붙은 또 다른 함수(fetchData)를 만들어서 사용해 주어야 한다.
-  useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true)
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${process.env.REACT_APP_NEWS_KEY}`)  // .env 파일 안에 있는 값은 리액트에서 REACT_APP_으로 시작되어야 인식이 된다. dotenv 따로 쓸 필요 없다.
-        setArticles(response.data.articles)
-      } catch (e) {
-        console.log(e)
-      }
-      setLoading(false)
-    }
-
-    fetchData()
+  const [loading, articles, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`
+    return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${process.env.REACT_APP_NEWS_KEY}`)
   }, [category])  // category 값이 바뀔 때마다 뉴스를 새로 불러와야 하기 때문에 useEffect의 의존 배열에 category를 넣어 주어야 한다. useEffect 한 번으로 컴포넌트가 맨 처음 렌더링될 때, 그리고 category 값이 바뀔 때 요청하도록 설정해줄 수 있다.
 
   if (loading) return <NewsListBlock>대기 중...</NewsListBlock>  // 대기 중일 때
-  if (!articles) return null                                     // 아직 articles 값이 설정 되지 않았을 때. map 함수를 사용하기 전에 꼭 !articles를 조회하여 해당 값이 현재 null인지 아닌지 검사해야 한다.
-                                                                 // 이 작업을 하지 않으면 아직 데이터가 없을 때 null에는 map 함수가 없기 때문에 렌더링 과정에서 오류가 발생한다. 그래서 애플리케이션이 제대로 나타나지 않고 흰 페이지만 보이게 된다.
+  if (!response) return null                                    // 아직 response 값이 설정되지 않았을 때
+                                                                // 이 작업을 하지 않으면 아직 데이터가 없을 때 null에는 map 함수가 없기 때문에 렌더링 과정에서 오류가 발생한다. 그래서 애플리케이션이 제대로 나타나지 않고 흰 페이지만 보이게 된다.
+  if (error) return <NewsListBlock>에러 발생!</NewsListBlock>    // 에러가 발생했을 때
 
-  // articles 값이 유효할 때
+
+  // response 값이 유효할 때
+  const { articles } = response.data
   return (
     <NewsListBlock>
       {articles.map(article => (  // 컴포넌트를 반환할 때는 {} 중괄호가 아닌 () 소괄호를 사용함에 유의하자.
--- src/lib/usePromise.js
+++ src/lib/usePromise.js
@@ -1,11 +1,13 @@
 // 컴포넌트에서 API 호출처럼 Promise를 사용해야 하는 경우 더욱 간결하게 코드를 작성할 수 있도록 해 주는 커스텀 Hook
 // 프로젝트의 다양한 곳에서 사용될 수 있는 유틸 함수들은 보통 이렇게 src 디렉터리에 lib 디렉터리를 만든 후 그 안에 작성한다.
 // usePromise Hook은 Promise의 대기 중, 완료 결과, 실패 결과에 대한 상태를 관리하며, usePromise의 의존 배열 deps를 파라미터로 받아 온다.
+// usePromise를 사용하면 NewsList에서 대기 중 상태관리와 useEffect 설저을 직접 하지 않아도 되므로 코드가 훨씬 간결해진다.
+// 요청 상태를 관리할 때 커스텀 Hook을 만들어서 사용하면 편리해진다.
 
 import { useState, useEffect } from 'react'
 
 export default function usePromise(promiseCreator, deps) {
-  // 대기 중/완료/실패에 대한 상태 관리
+  // 로딩중 / 완료 / 실패에 대한 상태 관리
   const [loading, setLoading] = useState(false)
   const [resolved, setResolved] = useState(null)
   const [error, setError] = useState(null)
@@ -22,7 +24,7 @@ export default function usePromise(promiseCreator, deps) {
     }
 
     process()
-  }, deps)  // deps 배열은 useEffect 의 두번째 파라미터로 전달되며, 기본값은 비어있는 배열. 비어있는 배열을 전달하면 컴포넌트가 가장 처음 렌더링 될 때만 실행 되는 것을 이용하여 가장 처음 렌더링 될 때만 실행되도록 함.
+  }, deps)  // deps 배열은 useEffect 의 두번째 파라미터로 전달된다. 여기로 [category] 값이 전달되어, category 값이 바뀔 때마다 뉴스를 새로 불러올 수 있게 된다. useEffect 한 번으로 컴포넌트가 맨 처음 렌더링될 때, 그리고 category 값이 바뀔 때 요청하도록 설정해줄 수 있다.
 
   return [loading, resolved, error]
 }
